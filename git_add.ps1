8b6147138c0bbf8d7b440d7a57958a369b59a27e
greeopia <greenpia033@gmail.com>
2026-09-08 19:00:06 +0800

    emm

--- git_add.ps1
+++ git_add.ps1
@@ -2,23 +2,26 @@
 $ErrorActionPreference = 'Stop'
 
 # Stage only ASCII date directories like 20260904 and 20260905-2.
-# The Chinese-named template was already staged manually.
 
 Set-Location -LiteralPath $PSScriptRoot
 
 $targets = Get-ChildItem -LiteralPath $PSScriptRoot -Directory |
-    Where-Object { $_.Name -match '^\d{8}(-\d+)?$' }
+#    Where-Object { $_.Name -match '^\d{8}(-\d+)?$' }
+    Where-Object { $_.Name -match '^\d{8}([._-]\d+)?$' }
 
 if (-not $targets) {
     Write-Host 'No date directories found.'
     exit 0
 }
 
-Write-Host 'Directories to stage:'
-$targets | Sort-Object Name | ForEach-Object { Write-Host ('  ' + $_.Name) }
-
-$targets | Sort-Object Name | ForEach-Object {
-    git add -- $_.FullName
+$targets | Sort-Object Name | ForEach-Object { 
+    # $hasChanges = git status --porcelain -- $_.FullName | Measure-Object | ForEach-Object { $_.Count -gt 0 }
+    $hasChanges = [bool](git status --porcelain -- $_.FullName)
+    if ($hasChanges) { git add -- $_.FullName }
 }
+
+# $targets | Sort-Object Name | ForEach-Object {
+#     git add -- $_.FullName
+# }
 
 git status --short
